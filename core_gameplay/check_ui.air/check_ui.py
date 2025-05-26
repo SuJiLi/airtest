8d18ce80f95e971fff7b53fb75377a6b7c65a298
su jili <840661624@qq.com>
2025-05-26 15:31:15 +0800

    “尝试修复图片路径问题”

--- core_gameplay/check_ui.air/check_ui.py
+++ core_gameplay/check_ui.air/check_ui.py
@@ -5,12 +5,12 @@ import sys
 sys.path.append(r"D:\PiaoFang_Test\core_gameplay")
 from airtest.core.settings import Settings as ST 
 from common import check_image1,check_image2,check_zhujiemian
-
+dev = connect_device("Android:///TPC7N18515001155")
 sleep(10)
 log_dir = r"D:\PiaoFang_Test\core_gameplay\check_ui_log"
 os.makedirs(log_dir, exist_ok=True)  # 确保目录存在
 set_logdir(log_dir)  # 强制指定日志位置
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.path.dirname(os.path.abspath(__file__))
 
 if exists(Template(r"tpl1747623526623.png")):
     touch(Template(r"tpl1747623606770.png", record_pos=(0.219, 0.716), resolution=(1080, 2240)))
